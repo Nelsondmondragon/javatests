d1704bcc27979e3eb0b3248f5725114f1a1c50b4
Nelson Cortes <nelsondmondragon@gmail.com>
2021-06-30 13:06:18 -0500

    Probando test Unitario, libreria JUnit

---
+++ src/test/java/com/cursoplatzi/javatest/util/PasswordUtilTest.java
@@ -0,0 +1,29 @@
+package com.cursoplatzi.javatest.util;
+
+import org.junit.Test;
+
+import static com.cursoplatzi.javatest.util.PasswordUtil.SecurityLevel.*;
+import static org.junit.Assert.*;
+
+public class PasswordUtilTest {
+
+    @Test
+    public void weak_when_has_less_than_8_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("12asd"));
+    }
+
+    @Test
+    public void weak_when_has_only_letters() {
+        assertEquals(WEAK, PasswordUtil.assessPassword("abcdcdcdcdcasdasd"));
+    }
+
+    @Test
+    public void medium_when_has_letters_and_numbers() {
+        assertEquals(MEDIUM, PasswordUtil.assessPassword("aasdbc233423424"));
+    }
+
+    @Test
+    public void strong_when_has_letters_numbers_and_symbols() {
+        assertEquals(STRONG, PasswordUtil.assessPassword("a23erwer42345sdfg!"));
+    }
+}
